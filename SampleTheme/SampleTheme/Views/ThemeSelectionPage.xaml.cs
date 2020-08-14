933a8cb9154235770b4221c62772c05852620c9f
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-08-14 17:19:36 +0530

    Changes are done

---
+++ SampleTheme/SampleTheme/Views/ThemeSelectionPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Linq;
+using Syncfusion.SfNumericTextBox.XForms;
+using Syncfusion.XForms.Themes;
+
+namespace SampleTheme
+{
+    public partial class ThemeSelectionPage : ContentPage
+    {
+        public ThemeSelectionPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+          
+            if (button.Text == "Light")
+            {
+                var lightTheme = mergedDictionaries.OfType<LightTheme>().FirstOrDefault();
+                if (lightTheme != null)
+                {
+                    mergedDictionaries.Remove(lightTheme);
+                }
+                mergedDictionaries.Add(new DarkTheme());
+
+                button.Text = "Dark";
+            }
+            else
+            {
+                var darkTheme = mergedDictionaries.OfType<DarkTheme>().FirstOrDefault();
+                if (darkTheme != null)
+                {
+                    mergedDictionaries.Remove(darkTheme);
+                }
+                mergedDictionaries.Add(new LightTheme());
+               
+                button.Text = "Light";
+            }
+        }
+    }
+
+}
